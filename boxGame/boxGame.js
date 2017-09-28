0ff06ac9c8082ec484f70375630a4d30dbda70d9
Danny <dwong38@gmail.com>
2017-09-27 22:09:14 -0700

    Create boxGame.js

---
+++ boxGame/boxGame.js
@@ -0,0 +1,41 @@
+var block = $("#box");
+var playAgain = $('#playAgain');
+var score = $('#score');
+var lost = $('#lost');
+var interval = 0;
+var number = 0;
+
+block.mouseenter(function() {
+	interval = setInterval(function () {
+		score.text("Score: " + number++);  
+	}, 100);
+});
+block.mouseleave(function() {
+	clearInterval(interval);
+	lost.show();
+	playAgain.show();
+	$('#lost').text("You lost...Final Score: " + (number-1));
+});
+
+playAgain.click(function() {
+	lost.hide();
+    playAgain.hide();
+	score.text("Score: 0");
+  	number = 0;
+
+});
+
+$('#easyMode').click(function() {
+	block.height(100);
+	block.width(100);
+});
+$('#intermediateMode').click(function() {
+	block.height(75);
+	block.width(75);
+});
+$('#hardMode').click(function() {
+	block.height(50);
+	block.width(50);
+});
+
+
